0cc1d5800024b60dc354a3e6cbfc97c7410dab4b
shuang.wang <ws156858@163.com>
2023-12-13 09:17:40 +0800

    validate txn

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const BC = require('./blockchain.js');
 const TX = require('./transaction.js');
+const ecLib = require("elliptic")
+
+const ec = new ecLib.ec("secp256k1")
 
 
 function main() {
     const utopia = new BC.Chain()
-    const t1 = new TX.Transaction('addr1', 'addr2', 1000)
-    const t2 = new TX.Transaction('addr2', 'addr3', 500)
-    utopia.addTxnIntoPool(t1)
-    utopia.addTxnIntoPool(t2)
-    utopia.mineTransactionPool('addr3')
-    console.log(utopia)
-    console.log(utopia.getlatestBlock().transactions)
+
+    const keyPair1 = ec.genKeyPair()
+    const pubKey1 = keyPair1.getPublic('hex')
+    const keyPair2 = ec.genKeyPair()
+    const pubKey2 = keyPair2.getPublic('hex')
+
+    const t1 = new TX.Transaction(pubKey1, pubKey2, 1000)
+    const t2 = new TX.Transaction(pubKey2, pubKey1, 500)
+    t1.sign(keyPair1)
+    console.log(t1.isValid())
+    
+    // utopia.addTxnIntoPool(t1)
+    // utopia.addTxnIntoPool(t2)
+    // utopia.mineTransactionPool('addr3')
+    // console.log(utopia)
+    // console.log(utopia.getlatestBlock().transactions)
 }
 
 main()
--- transaction.js
+++ transaction.js
@@ -1,11 +1,31 @@
+const sha256 = require("crypto-js/sha256")
+const ecLib = require("elliptic")
+
+const ec =  new ecLib.ec("secp256k1")
+
 class Transaction {
-    constructor(from, to, amount) {
-        this.from = from
-        this.to = to
-        this.amount = amount
-    }
+  constructor(from, to, amount) {
+    this.from = from
+    this.to = to
+    this.amount = amount
+    this.signature = ""
+  }
+
+  // 计算这笔交易的哈希
+  computeHash() {
+    return sha256(this.from + this.to + this.amount).toString()
+  }
+  // 用from的私钥对交易哈希进行签名
+  sign(fromKeyPair) {
+    this.signature = fromKeyPair.sign(this.computeHash(), "base64").toDER("hex")
+  }
+  // 把交易的签名用from公钥进行解密，然后将解密后的结果与给定的哈希值进行比较
+  isValid() {
+    const fromKeyPair = ec.keyFromPublic(this.from, "hex") // 这里为了简单, 认定地址就是公钥
+    return fromKeyPair.verify(this.computeHash(), this.signature)
+  }
 }
 
 module.exports = {
-    Transaction: Transaction,
+  Transaction: Transaction,
 }
